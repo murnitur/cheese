3d3784c24493a017aebe58ff00296989115b2469
Caleb <caleb@git.com>
2019-09-17 21:38:24 -0700

    version 1.3.0 finally done

--- app/loaders/helpers/load.php
+++ app/loaders/helpers/load.php
@@ -6,6 +6,5 @@
     include APPROOT.'/helpers/session_helper.php';
     include APPROOT.'/helpers/url_helper.php';
     include APPROOT.'/helpers/string_helper.php';
-    include APPROOT.'/helpers/number_helper.php';
     include APPROOT.'/helpers/URL.php';
     include APPROOT.'/helpers/function.php';
